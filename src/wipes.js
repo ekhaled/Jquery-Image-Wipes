b57771a43d7d3ab878a43d6878a6e26b56a87052
khaled <macmini@graeme-newmans-mac-mini-2.local>
2010-03-02 17:11:35 +0000

    animation, easing and wipes core in place

---
+++ src/wipes.js
@@ -0,0 +1,11 @@
+(function($){
+	$.fn.wipeImages.wipes={
+		verticalStrips:{},
+		horizontalStrips:{},
+		topBottomWipe:{},
+		bottomTopWipe:{},
+		leftRightWipe:{},
+		rightLeftWipe:{}
+	}
+	
+})(jQuery);
