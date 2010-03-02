fbd0605b114d9b73f984634d857cfd19dec2f247
khaled <khaled@khaled-laptop.(none)>
2010-03-02 21:51:46 +0000

    setup function in core.js completed

--- src/core.js
+++ src/core.js
@@ -6,21 +6,41 @@
 	
 	function init(config){
 		$.fn.wipeImages.config=$.extend(config,$.fn.wipeImages.config);
-		setupCss(this);
+		setup(this);
 	}
 	
-	function setupCss(els){
+	function setup(els){
 		els.each(function(){
 			var _this=this;
+			//find all images in this
+			var $imgs=$("img:visible",_this)
+			.css({ // position the images
+				position:"absolute",
+				width:"100%",
+				height:"100%"
+			});
+			//set up height, width and position of parent
+			$(_this).css({
+				width:$.fn.wipeImages.config.viewportWidth + "px",
+				height:$.fn.wipeImages.config.viewportHeight + "px",
+				position:"relative"
+			})
+			.empty() //clear all contents
+			.append($imgs) // add the filtered images
+			.children(":gt(0)").hide() //hide all except the first one
+			.end()
+			.append("<div class='imageWipe_overlay' style='position:absolute;width:100%;height:100%'></div>");
+			//add overlay div and position it
 		});
 	}
 	
 	$.fn.wipeImages.config={ // configuration object
-		viewportWidth:"300px", // width of the viewport
-		viewportHeight:"300px", // height of the viewport
+		viewportWidth:600, // width of the viewport in px
+		viewportHeight:400, // height of the viewport in px
 		useWipes:"random", // accepts "random" or array of wipes e.g ["verticalStrips","rightLeftWipe"]
 		delay:500, // delay between wipes
-		duration:300 //duration of individual wipes
+		duration:300, //duration of individual wipes
+		autoPlay:true
 	};
 
 
