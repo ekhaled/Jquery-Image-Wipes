55023c5a14c712b953a39f5c4e26580b13bf756c
ekhaled <me.khaled@gmail.com>
2011-05-15 02:13:44 +0100

    whitespace

--- src/extendedWipes.js
+++ src/extendedWipes.js
@@ -1,39 +1,40 @@
 (function($){
-   function box2D(r,c,rev){
-      var grouped=[],arr=[],b=0;
-      
-      for(var i = 0; i < r; i++) {
-        arr[i] = new Array(c);
-        for(var j = 0; j < c; j++) {
-            arr[i][j] = b;
-            b++;
-        }
+
+  function box2D(r,c,rev){
+    var grouped=[],arr=[],b=0;
+    
+    for(var i = 0; i < r; i++) {
+      arr[i] = new Array(c);
+      for(var j = 0; j < c; j++) {
+          arr[i][j] = b;
+          b++;
       }
-      
-      if(rev){
-        for (var i = 1 - r; i < c; i++) {
-				  var group = [];
-				  for (var j = 0; j < r; j++) {
-					  if ((i + j) >= 0 && (i + j) < c) {
-						  group.push(arr[j][i + j]);
-					  }
-				  }
-				  grouped.push(group);
-			  }
-      }else{
-        for (var i = 0; i < r + c - 1; i++) {
-          var z1 = (i < c) ? 0 : i - c + 1;
-          var z2 = (i < r) ? 0 : i - r + 1;
-          var group=[];
-          for (var j = i - z2; j >= z1; j--) {
-            group.push(arr[j][i - j]);
-          }
-          grouped.push(group);
-        }
-      }
-     
-      return grouped;
     }
+    
+    if(rev){
+      for (var i = 1 - r; i < c; i++) {
+		    var group = [];
+		    for (var j = 0; j < r; j++) {
+			    if ((i + j) >= 0 && (i + j) < c) {
+				    group.push(arr[j][i + j]);
+			    }
+		    }
+		    grouped.push(group);
+	    }
+    }else{
+      for (var i = 0; i < r + c - 1; i++) {
+        var z1 = (i < c) ? 0 : i - c + 1;
+        var z2 = (i < r) ? 0 : i - r + 1;
+        var group=[];
+        for (var j = i - z2; j >= z1; j--) {
+          group.push(arr[j][i - j]);
+        }
+        grouped.push(group);
+      }
+    }
+   
+    return grouped;
+  }
 
 
   var ns=$.fn.wipeImages,factory=ns.wipeFactory;
